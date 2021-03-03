6448a98f9b0a58c86515d8a328c656b8513ef13d
Andy Taylor <andy@extramilecommunications.com>
2021-03-03 18:11:49 +0000

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Page</title>
+            <h1>About Page</h1>
+            <p>This is the about page of the example website</p>
+        </main>
+    )
+}
+
+export default AboutPage;
